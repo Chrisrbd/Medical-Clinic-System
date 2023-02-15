064c47e50c92f1e4a4a88a6c34a71045f764c0be
ArTranArt <120374187+ArTranArt@users.noreply.github.com>
2023-02-15 14:19:13 -0500

    Update Doctor.java

--- Doctor.java
+++ Doctor.java
@@ -1,11 +1,30 @@
-import java.util.Date;
+import java.time.LocalDate;
 
-public class Doctor extends Person{
-    
-    private String speciality;
-
-    public Doctor(String name, Date birthdate, String speciality){
-        super(name, birthdate);
-        this.speciality = speciality;
+public class Doctor extends Person {
+    private String specialty;
+  
+    public Doctor(String id, String name, LocalDate dateOfBirth, String address, String phoneNumber, String specialty) {
+      super(id, name, dateOfBirth, address, phoneNumber);
+      this.specialty = specialty;
     }
-}
+  
+    public String getSpecialty() {
+      return specialty;
+    }
+  
+    public void setSpecialty(String specialty) {
+      this.specialty = specialty;
+    }
+  
+    @Override
+    public String toString() {
+        return  
+        "id: " + getId() +
+        "name: " + getName() +
+        "\ndateOfBirth: " + getDateOfBirth() +
+        "\naddress: " + getAddress() +
+        "\nphoneNumber: " + getPhoneNumber() +
+        "\nspecialty: " + specialty
+        ;
+    }
+  }
