3f058b961a03853a3ba691298f34da5b800c4c88
chrisrbd <christophe.reybaud@efrei.net>
2023-02-15 14:44:21 -0500

    while exit != true

--- main.java
+++ main.java
@@ -31,52 +31,63 @@ public class Main {
 
         //final part
         int numAffichage; 
-        Scanner sc = new Scanner(System.in); 
-        System.out.println("Welcome to our Medical Clinic !");
-        System.out.println("Please enter your role:");
-        System.out.println("1) I am a Patient");
-        System.out.println("2) I am a Doctor");
-        System.out.println("3) I am the Receptionist ");
-        numAffichage = sc.nextInt(); 
-        switch (numAffichage) {
-            case 1 : {
-                System.out.println("What do you want to do ?");
-                System.out.println("1) Change my personal informations");
-                break;
-            }
-            case 2 : {
-                System.out.println("What do you want to do ?");
-                System.out.println(":p");
-                break;
-            }
-            case 3 : {
-                System.out.println("What do you want to do ?");
-                System.out.println("1) List all doctors");
-                System.out.println("2) List all patients");
-                numAffichage = sc.nextInt();
-                switch (numAffichage) {
-                    case 1 : {        
-                        // Print out the list of Doctor objects
-                        System.out.println("Here are all the doctors :");
-                        for (Doctor doctor : doctorList) {
-                            System.out.println(doctor+"\n");
-                        }
-                        System.out.println("****************************************************");
-                        break;
-                    }
-                    case 2 : {        
-                        // Print out the list of Patient objects
-                        System.out.println("Here are all the patients :");
-                        for (Patient patient : patientList) {
-                            System.out.println(patient+"\n");
-                        }
-                        System.out.println("****************************************************");
-                        break;
-                    }
+        Scanner sc = new Scanner(System.in);
+        boolean exit = false;
+        while(exit != true){
+            System.out.println("\nWelcome to our Medical Clinic !");
+            System.out.println("\nPlease enter your role:");
+            System.out.println("1) I am a Patient");
+            System.out.println("2) I am a Doctor");
+            System.out.println("3) I am the Receptionist ");
+            System.out.println("4) Exit ");
+            System.out.print("\nYour choice: ");
+            numAffichage = sc.nextInt(); 
+            switch (numAffichage) {
+                case 1 : {
+                    System.out.println("What do you want to do ?");
+                    System.out.println("1) Change my personal informations");
+                    break;
                 }
-                break;
+                case 2 : {
+                    System.out.println("What do you want to do ?");
+                    System.out.println(":p");
+                    break;
+                }
+                case 3 : {
+                    System.out.println("What do you want to do ?");
+                    System.out.println("1) List all doctors");
+                    System.out.println("2) List all patients");
+                    numAffichage = sc.nextInt();
+                    switch (numAffichage) {
+                        case 1 : {        
+                            // Print out the list of Doctor objects
+                            System.out.println("Here are all the doctors :");
+                            for (Doctor doctor : doctorList) {
+                                System.out.println(doctor+"\n");
+                            }
+                            System.out.println("****************************************************");
+                            break;
+                        }
+                        case 2 : {        
+                            // Print out the list of Patient objects
+                            System.out.println("Here are all the patients :");
+                            for (Patient patient : patientList) {
+                                System.out.println(patient+"\n");
+                            }
+                            System.out.println("****************************************************");
+                            break;
+                        }
+                    }
+                    
+                }
+                case 4 : {
+                    System.out.println("Goodbye !");
+                    exit = true;
+                    break;
+                }
             }
+            sc.close();
         }
-        sc.close();
+        
     }
 }
