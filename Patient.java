9139d9192bb127fbb186b275073ee42cd6c6cc6d
ArTranArt <120374187+ArTranArt@users.noreply.github.com>
2023-02-15 14:18:52 -0500

    Update Patient.java

--- Patient.java
+++ Patient.java
@@ -1,11 +1,30 @@
-import java.util.Date;
+import java.time.LocalDate;
 
 public class Patient extends Person {
     private String insuranceCompany;
-
-    public Patient(String name, Date birthdate, String insuranceCompany){
-        super(name, birthdate);
-        this.insuranceCompany = insuranceCompany;
+  
+    public Patient(String id, String name, LocalDate dateOfBirth, String address, String phoneNumber, String insuranceCompany) {
+      super(id, name, dateOfBirth, address, phoneNumber);
+      this.insuranceCompany = insuranceCompany;
     }
-    
-}
+  
+    public String getInsuranceCompany() {
+      return insuranceCompany;
+    }
+  
+    public void setInsuranceCompany(String insuranceCompany) {
+      this.insuranceCompany = insuranceCompany;
+    }
+  
+    @Override
+    public String toString() {
+        return  
+        "id: " + getId() +
+        "name: " + getName() +
+        "\ndateOfBirth: " + getDateOfBirth() +
+        "\naddress: " + getAddress() +
+        "\nphoneNumber: " + getPhoneNumber() +
+        "\ninsurance company: " + getInsuranceCompany()
+        ;
+    }
+  }
