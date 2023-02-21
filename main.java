a82c2900e26423e7dc6f92afa06920ba4124089b
Christophe <118166861+Chrisrbd@users.noreply.github.com>
2023-02-21 12:40:58 -0500

    Update main.java

--- main.java
+++ main.java
@@ -79,84 +79,94 @@ public class Main {
                         break;
                     }
                     case 'D':{ // Doctor
-                        System.out.println("What do you want to do ?");
-                        System.out.println("\n1) Change my personal information");
-                        System.out.println("2) Add a treatment");
-                        System.out.println("3) Modify a treatment");
-                        System.out.println("4) Delete a treatment");
-                        System.out.println("5) List all my treatments");
-                        System.out.println("6) Exit");
-                        System.out.print("\nYour choice: ");
-                        numMenu = input.nextInt();
-                        switch(numMenu){
-                            case 1 : {
-                                System.out.println("Change my personal information");
-                                
-                                break;
-                            }
-                            case 2 : {
-                                Appointment.addAppointment();
-                                break;
-                            }
-                            case 3 : {
-                                Treatment.addTreatment();
-                                break;
-                            }
-                            case 4 : {
-                                break;
-                            }
-                            case 5 : {
-                                break;
-                            }
-                            case 6 : {
-                                System.out.println("\nThank you for using our Medical Clinic System. Goodbye !\n");
-                                exit = true;
-                                break;
+                        while(exit !=true){
+                            System.out.println("What do you want to do ?");
+                            System.out.println("\n1) Change my personal information");
+                            System.out.println("2) Add a treatment");
+                            System.out.println("3) Modify a treatment");
+                            System.out.println("4) Delete a treatment");
+                            System.out.println("5) List all treatments");
+                            System.out.println("6) Exit");
+                            System.out.print("\nYour choice: ");
+                            numMenu = input.nextInt();
+                            switch(numMenu){
+                                case 1 : {
+                                    System.out.println("Change my personal information");
+                                    
+                                    break;
+                                }
+                                case 2 : {
+                                    Appointment.addAppointment();
+                                    break;
+                                }
+                                case 3 : {
+                                    Treatment.addTreatment();
+                                    break;
+                                }
+                                case 4 : {
+                                    break;
+                                }
+                                case 5 : {
+                                    System.out.println("Here are all the treatments :");
+                                    PrintFile.printFile("Treatments.txt");
+                                    break;
+                                }
+                                case 6 : {
+                                    System.out.println("\nThank you for using our Medical Clinic System. Goodbye !\n");
+                                    exit = true;
+                                    break;
+                                }
                             }
                         }
                         break;
                     }
                     case 'R':{ // Receptionist
-                        System.out.println("What do you want to do ?");
-                        System.out.println("\n1) Change my personal information");
-                        System.out.println("2) List all doctors");
-                        System.out.println("3) List all patients");
-                        System.out.println("4) Add an Appointment");
-                        System.out.println("5) Cancel an appointment");
-                        System.out.println("6) Modify an appointment");
-                        System.out.println("7) Exit");
-                        numMenu = input.nextInt();
-                        switch (numMenu) {
-                            case 2 : {        
-                                // Print out the list of Doctor objects
-                                System.out.println("Here are all the doctors :");
-                                
-                                System.out.println("****************************************************");
-                                break;
-                            }
-                            case 3 : {        
-                                // Print out the list of Patient objects
-                                System.out.println("Here are all the patients :");
-
-                                System.out.println("****************************************************");
-                                break;
-                            }
-                            case 4 : {
-                                Appointment.addAppointment();
-                                break;
-                            }
-                            case 5 : {
-                                System.out.println("Cancel an appointment");
-                                break;
-                            }
-                            case 6 : {
-                                System.out.println("Modify an appointment");
-                                break;
-                            }
-                            case 7 : {
-                                System.out.println("\nThank you for using our Medical Clinic System. Goodbye !\n");
-                                exit = true;
-                                break;
+                        while(exit != true){
+                            System.out.println("What do you want to do ?");
+                            System.out.println("\n1) Change my personal information");
+                            System.out.println("2) List all doctors");
+                            System.out.println("3) List all patients");
+                            System.out.println("4) Add an Appointment");
+                            System.out.println("5) Cancel an appointment");
+                            System.out.println("6) Modify an appointment");
+                            System.out.println("7) List all appointments");
+                            System.out.println("8) Exit");
+                            numMenu = input.nextInt();
+                            switch (numMenu) {
+                                case 2 : {        
+                                    // Print out the list of Doctor objects
+                                    System.out.println("Here are all the doctors :");
+                                    PrintFile.printFile("Doctors.txt");
+                                    break;
+                                }
+                                case 3 : {        
+                                    // Print out the list of Patient objects
+                                    System.out.println("Here are all the patients :");
+                                    PrintFile.printFile("Patients.txt");
+                                    break;
+                                }
+                                case 4 : {
+                                    Appointment.addAppointment();
+                                    break;
+                                }
+                                case 5 : {
+                                    System.out.println("Cancel an appointment");
+                                    break;
+                                }
+                                case 6 : {
+                                    System.out.println("Modify an appointment");
+                                    break;
+                                }
+                                case 7 : {
+                                    System.out.println("Here are all the appointments :");
+                                    PrintFile.printFile("Appointments.txt");
+                                    break;
+                                }
+                                case 8 : {
+                                    System.out.println("\nThank you for using our Medical Clinic System. Goodbye !\n");
+                                    exit = true;
+                                    break;
+                                }
                             }
                         }
                         break;
