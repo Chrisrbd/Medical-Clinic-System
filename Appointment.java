63336074eaf053f32ce39cbdcdf31f179996a0b6
Christophe <118166861+Chrisrbd@users.noreply.github.com>
2023-02-21 12:24:30 -0500

    Update Appointment.java

--- Appointment.java
+++ Appointment.java
@@ -33,10 +33,22 @@ public class Appointment {
         String doctor = input.nextLine();
         System.out.println("Enter the reason for the appointment: ");
         String reason = input.next();
-        System.out.println("Enter the date of the appointment (yyyy-mm-dd): ");
-        LocalDate date = LocalDate.parse(input.next());
-        System.out.println("Enter the time of the appointment (hh:mm): ");
-        LocalTime time = LocalTime.parse(input.next());
+        System.out.print("Enter the date of the appointment (yyyy-mm-dd): ");
+        String newDate = input.next();
+        while (!newDate.matches("\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2]\\d|3[01])")) {
+            System.out.println("Invalid date. Please enter a date in the correct format.");
+            System.out.print("Enter the date of the appointment (yyyy-mm-dd): ");
+            newDate = input.next();
+        }
+        LocalDate date = LocalDate.parse(newDate);
+        System.out.print("Enter the time of the appointment (hh:mm): ");
+        String newTime = input.next();
+        while (!newTime.matches("([01]?[0-9]|2[0-3]):[0-5][0-9]")) {
+            System.out.println("Invalid time. Please enter a time in the correct format.");
+            System.out.print("Enter the time of the appointment (hh:mm): ");
+            newTime = input.next();
+        }
+        LocalTime time = LocalTime.parse(newTime);
         Appointment newAppointment = new Appointment(patient, doctor, reason, date, time);
         System.out.println("Appointment added successfully: ");
         System.out.println(newAppointment);
